6f3dfecd58b75a11a559b567ad9218a5523dae90
RaihanAk <raihandoank@ymail.com>
2020-02-20 14:34:06 +0700

    init commit

---
+++ Assets/Script/CubeHitbox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHitbox : MonoBehaviour {
+
+	public int speed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Time.deltaTime * transform.forward * speed;	
+	}
+}
---
+++ Assets/Script/SpawnerHitbox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerHitbox : MonoBehaviour {
+
+	public GameObject[] cubes;
+	public Transform[] points;
+	public float beat;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		beat *= 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > beat){
+			GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+			cube.transform.localPosition = Vector3.zero;
+			cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+			timer -= beat;
+		}
+
+		timer += Time.deltaTime;
+	}
+}

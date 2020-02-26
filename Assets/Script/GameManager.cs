7899f7e1f43b10d8db9d08027b54a993b51bb9b8
RaihanAk <raihanakbar1999@gmail.com>
2020-02-26 16:26:28 +0700

    implement scoring

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    // Hitboxes
+	public GameObject[] cubes;
+    // spawnpoints
+	public Transform[] points;
+    // Song to play
+    public AudioSource song;
+
+    // Song's BPM
+    public float beatPerMinute;
+
+    // Timer for time progression
+    private float timer;
+    private bool isPlaying = false;
+    private int score = 0;
+
+    void Start()
+    {
+        PlaySaber();
+    }
+
+    void PlaySaber()
+    {
+        beatPerMinute /= 210;
+        isPlaying = true;
+        song.Play(0);
+    }
+
+    void FixedUpdate () {
+        if (isPlaying)
+        {
+            if (timer > beatPerMinute && song.isPlaying)
+            {
+                GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0, 4)]);
+                cube.transform.localPosition = Vector3.zero;
+                cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));
+                timer -= beatPerMinute;
+            }
+
+            timer += Time.deltaTime;
+
+            if (!song.isPlaying)
+            {
+                EndGame();
+            }
+        }
+	}
+
+    public void EndGame()
+    {
+        isPlaying = false;
+        Debug.Log("Selesai coi, score: " + score);
+    }
+
+    public void ScoreModify(int n)
+    {
+        score += n;
+    }
+}
--- Assets/Script/Saber.cs
+++ Assets/Script/Saber.cs
@@ -7,19 +7,20 @@ public class Saber : MonoBehaviour {
 	public LayerMask layer;
 	private Vector3 previousPos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameManager gameManager;
 	
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer)){
-			if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130){
-				Destroy(hit.transform.gameObject);
-			}
-		}	
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                gameManager.ScoreModify(3);
+                Destroy(hit.transform.gameObject);
+            }
+            else
+                gameManager.ScoreModify(1);            
+        }	
 
 		previousPos = transform.position;
 	}
